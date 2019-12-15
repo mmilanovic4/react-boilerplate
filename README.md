32dcdf1e0809d8cf15ad95637e4874a4fb5af86a
Miloš Milanović <mmilanovic016@gmail.com>
2019-12-15 02:51:23 +0100

    Major changes
    - Change project structure
    - Remove webpack.dev.js and webpack.prod.js - have only one webpack script and pass environment information via webpack env
    - Move HTML files to separate folder
    - Move other static files to seprate folder
    - Update docs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <p align="center">
-	<img src="public/logo.png" alt="React boilerplate">
+	<img src="static/logo.png" alt="React boilerplate">
 </p>
 
 # Overview
@@ -14,20 +14,32 @@ cd react-boilerplate
 npm install
 ```
 
-## Development
-
-```
-npm run start:dev
-```
-
 ## Production
 
 ```
-npm run-script build
+npm run clean
+npm run build:prod
 ```
 
-You can test production build on your development workstation by running:
+## Development
+
+You can use this boilerplate with your own server (e.g. Express), then you can just run build script with development flag:
 
 ```
-npm run start:prod
+npm run build:dev
+```
+
+Difference between `build:prod` and `build:dev`:
+
+- `webpack.mode` is set to `production` if started via `build:prod` or to `development` if started via `build:dev`
+- `webpack.watch` is set to `true` if started via `build:dev`
+
+To serve this app via `webpack-dev-server` you simply run:
+
+```
+# First terminal
+npm run build:dev
+
+# Second terminal
+npm run start:dev
 ```
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = (env) => {
+	const isProd = env && env.prod;
+
+	return {
+		mode: isProd ? 'production' : 'development',
+		watch: !isProd,
+		entry: {
+			app: [
+				'core-js/stable',
+				'regenerator-runtime/runtime',
+				'./src/client/index.js'
+			]
+		},
+		resolve: {
+			extensions: ['.js', '.jsx']
+		},
+		output: {
+			filename: '[name].js',
+			path: path.resolve(__dirname, 'dist', 'assets'),
+			publicPath: 'assets/'
+		},
+		module: {
+			rules: [
+				// Babel
+				{
+					test: /\.jsx?$/,
+					use: 'babel-loader',
+					exclude: /node_modules/
+				},
+				// CSS
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
+				},
+				// Images
+				{
+					test: /\.(jpg|jpeg|png|gif|webp)$/,
+					use: {
+						loader: 'file-loader',
+						options: {
+							name: 'img/[name].[ext]'
+						}
+					}
+				},
+				// Fonts
+				{
+					test: /\.(eot|otf|svg|ttf|woff|woff2)$/,
+					use: {
+						loader: 'file-loader',
+						options: {
+							name: 'fonts/[name].[ext]'
+						}
+					}
+				}
+			]
+		},
+		plugins: [
+			new CopyPlugin([
+				{ from: path.resolve(__dirname, 'html'), to: path.resolve(__dirname, 'dist') },
+				{ from: path.resolve(__dirname, 'static'), to: path.resolve(__dirname, 'dist', 'static') }
+			])
+		]
+	}
+};
