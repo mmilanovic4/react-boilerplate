4e6057883de9c452e71e21d4b493b4d974a041ed
Miloš Milanović <mmilanovic016@gmail.com>
2020-07-28 17:21:57 +0200

    Minor refactor

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -3,9 +3,5 @@ import React from 'react';
 import Calculator from 'Client/components/Calculator';
 
 export default () => {
-	return (
-		<>
-			<Calculator />
-		</>
-	);
+	return <Calculator />;
 };
