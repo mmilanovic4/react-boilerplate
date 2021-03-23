28924a2c41533a85fcf84986f0307e751ce1ee51
Miloš Milanović <mmilanovic016@gmail.com>
2021-03-23 21:01:06 +0100

    Move Babel config to Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -34,8 +34,14 @@ module.exports = (env) => {
 				// Babel
 				{
 					test: /\.jsx?$/,
-					use: 'babel-loader',
-					exclude: /node_modules/
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env', '@babel/preset-react'],
+							plugins: ['@babel/plugin-proposal-class-properties']
+						}
+					}
 				},
 				// CSS
 				{
