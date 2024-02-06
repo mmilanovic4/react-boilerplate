d377cd3f4ff15f9a0ea0c0aabe196de5683011b3
Miloš Milanović <mmilanovic016@gmail.com>
2024-02-06 19:52:04 +0100

    Modify Babel config

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,10 @@ module.exports = (env) => {
 					use: {
 						loader: 'babel-loader',
 						options: {
-							presets: ['@babel/preset-env', '@babel/preset-react'],
+							presets: [
+								'@babel/preset-env',
+								['@babel/preset-react', { runtime: 'automatic' }]
+							],
 							plugins: ['@babel/plugin-proposal-class-properties']
 						}
 					}
